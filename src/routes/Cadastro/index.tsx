f0d00da2a23b6de9c906bfcb5277bee952187525
biribillidev <biribillijoaovitor@gmail.com>
2025-11-12 12:57:01 -0300

    feat: adicionando rota cadastro

--- src/routes/Cadastro/index.tsx
+++ src/routes/Cadastro/index.tsx
@@ -1,32 +1,64 @@
+import { useForm } from "react-hook-form"
+
+
+function onSubmit() {
+    alert('Cadastro realizado com sucesso!')
+  }
+
 export default function Cadastro(){
+
+    const {register, handleSubmit} = useForm()
+
     return(
         <div className="formCadastro">
+        
         <h1> Cadastre-se </h1>
         <p>Preencha os campos abaixo para se cadastrar</p>
+        
+        <form onSubmit={handleSubmit(onSubmit)}>
+            
         <div className="labelCadastro">
-            <label htmlFor="form-label"> Nome: </label>
-            <input type="text" 
-            required
-            />
-        </div>
-        <div className="labelCadastro">
-            <label htmlFor="form-label"> Email:</label>
-            <input type="email"
-            required
-            />
-        </div>
-        <div className="labelCadastro">
-            <label htmlFor="form-label"> Senha:</label>
-            <input type="password" 
-            required
-            />
-        </div>
-        <div className="labelCadastro">
-            <label htmlFor="text"> Area de Interesse: </label>
-            <input type="text" 
-            required
-            />
-        </div>
+                <label htmlFor="form-label"> Nome: </label>
+                <input type="text"
+                {...register("nome",{
+                    required: "Nome é obrigatório",
+                    minLength: { value: 3, message: "O nome deve ter pelo menos 3 caracteres." }
+                })} 
+                />
+            </div>
+
+            <div className="labelCadastro">
+                <label htmlFor="form-label"> Email:</label>
+                <input type="email"
+                {...register("email",{
+                    required: "Email é obrigatório",
+                    pattern:{
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                        message: "Insira um email válido" 
+                    }
+                })}
+                />
+            </div>
+            <div className="labelCadastro">
+                <label htmlFor="form-label"> Senha:</label>
+                <input type="password" 
+                {...register("senha", {
+                    required: "Senha é obrigatória",
+                    minLength: {value: 8, message: "A senha deve ter pelo menis 8 caracteres"},
+                    pattern: {
+                        value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/,
+                        message: "A senha deve conter pelo menos uma letra maiúscula, uma minúscula e um número"
+                    }
+                })}
+                />
+            </div>
+            <div className="labelCadastro">
+                <label htmlFor="text"> Area de Interesse: </label>
+                <input type="text" 
+                required
+                />
+            </div>
+        </form>
         </div>
     )
 }
