d4ff0597664c11d057fab80462734ec1b9d659ba
biribillidev <biribillijoaovitor@gmail.com>
2025-11-12 14:04:28 -0300

    feat: adicionando a rota nas páginas

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -4,6 +4,7 @@ export default function Menu() {
     return(
         <nav className="menu">
             <Link to="/">Inicio</Link>
+            <Link to="/quemsomos">Quem Somos</Link>
         </nav>
     )
 }
--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -3,5 +3,5 @@ export default function Error(){
         <main>
             <h1>Not Found - Erro 404</h1>
         </main>
-    );
+    )
 }
