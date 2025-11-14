24127eb6c03ecc73d2c94fd95fea304d9710e0d2
PietroWilhelm <pietrowilhelm@git.com>
2025-11-14 13:02:53 -0300

    feat: componentes Cabecalho e Menu para melhororados para  organização e responsividade, além da adição do menu hamburger

--- src/components/Cabecalho/cabecalho.tsx
+++ src/components/Cabecalho/cabecalho.tsx
@@ -4,18 +4,16 @@ import Menu from "../Menu/Menu"
 export default function Cabecalho() {
     return(
         <header className="bg-linear-to-r from-blue-600 to-purple-600 shadow-lg sticky top-0 z-50">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex items-center justify-between h-20">
-                    {/* Logo */}
-                    <div>
-                        <h1 className="text-3xl font-bold text-white tracking-tight hover:scale-105 transition-transform cursor-pointer">
-                            SkillUp
-                        </h1>
-                    </div>
-                    
-                    {/* Menu */}
-                    <Menu/>
+            <div className="relative flex items-center justify-between h-20 px-4 sm:px-6 lg:px-12">
+                {/* Logo */}
+                <div>
+                    <h1 className="text-2xl sm:text-3xl font-bold text-white tracking-tight hover:scale-105 transition-transform cursor-pointer">
+                        SkillUp
+                    </h1>
                 </div>
+                
+                {/* Menu */}
+                <Menu/>
             </div>
         </header>
     );
