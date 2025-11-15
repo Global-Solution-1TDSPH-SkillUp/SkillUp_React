78467799f96aa0d18db6dea081423ad5876a4413
PietroWilhelm <pietrowilhelm@git.com>
2025-11-15 15:30:17 -0300

    chore: ocultei os links do header nas paginas login e cadastro utilizando de Location para obter a rota atual

--- src/components/Cabecalho/cabecalho.tsx
+++ src/components/Cabecalho/cabecalho.tsx
@@ -1,7 +1,12 @@
 import Menu from "../Menu/Menu"
+import { useLocation } from "react-router-dom"
 
 
 export default function Cabecalho() {
+    const location = useLocation() // Acesso a localização atual da rota
+    const rotasSemMenu = ['/login', '/cadastro'] //crio uma lista de  Rotas onde o menu não deve ser exibido
+    const mostrarMenu = !rotasSemMenu.includes(location.pathname) // Verifico se a rota atual está na lista de rotas sem menu
+
     return(
         <header className="bg-linear-to-r from-blue-600 to-purple-600 shadow-lg sticky top-0 z-50">
             <div className="relative flex items-center justify-between h-20 px-4 sm:px-6 lg:px-12">
@@ -13,7 +18,7 @@ export default function Cabecalho() {
                 </div>
                 
                 {/* Menu */}
-                <Menu/>
+                {mostrarMenu && <Menu/>}
             </div>
         </header>
     );
